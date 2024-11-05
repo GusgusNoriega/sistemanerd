6aaa98324474a2300f78ffb73ce1c3ff3571678a
Gustavo <gusgusnoriega@gmail.com>
2024-11-05 12:09:19 -0500

    Agrega ruta y controlador para obtener datos de usuario

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +16,12 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    
+    // Otras rutas protegidas
 });
+
+Route::get('/user-data/{id}', [UserController::class, 'getUserData']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
